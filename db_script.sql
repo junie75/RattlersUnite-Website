d7557e4d9f9e3afcebe1124540338b50194d5854
GanstaKingofSA <azarieldc@gmail.com>
2023-04-24 03:26:03 -0500

    purge some leftovers

--- db_script.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS Events;
-
-CREATE TABLE Events (
-    name TEXT NOT NULL,
-    organization TEXT NOT NULL,
-    eventDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    eventDescription TEXT NOT NULL
-);
